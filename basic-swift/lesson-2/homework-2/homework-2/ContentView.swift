4997b3f6fd62fc230495429a0b68c36f008f0b54
Marat <marat.khanbekov@gmail.com>
2020-07-01 21:22:59 +0300

    [lesson-2] Homework init commit

---
+++ basic-swift/lesson-2/homework-2/homework-2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  homework-2
+//
+//  Created by Marat on 01.07.2020.
+//  Copyright © 2020 Marat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+    
+//    for i in (1...10) {
+//    print(i)
+//    }
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
